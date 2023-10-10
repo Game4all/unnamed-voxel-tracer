42df88f44de5360f0be067ee00447e45827e1834
Lucas A <game4allyt@gmail.com>
2023-10-10 21:41:08 +0200

    abc

--- assets/shaders/sdf.glsl
+++ assets/shaders/sdf.glsl
@@ -16,3 +16,13 @@ float sdBox( vec3 p, vec3 orig, vec3 b )
   vec3 q = abs(p - orig) - b;
   return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
 }
+
+float sdBoxFrame( vec3 p, vec3 orig, vec3 b, float e )
+{
+       p = abs(p - orig)-b;
+  vec3 q = abs(p - orig + e)-e;
+  return min(min(
+      length(max(vec3(p.x,q.y,q.z),0.0))+min(max(p.x,max(q.y,q.z)),0.0),
+      length(max(vec3(q.x,p.y,q.z),0.0))+min(max(q.x,max(p.y,q.z)),0.0)),
+      length(max(vec3(q.x,q.y,p.z),0.0))+min(max(q.x,max(q.y,p.z)),0.0));
+}
