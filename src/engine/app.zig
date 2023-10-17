56f6cf34b4e486c4ebcf01de669e7f4ae948fabf
Lucas A <game4allyt@gmail.com>
2023-10-17 09:35:25 +0300

    App

--- src/engine/app.zig
+++ src/engine/app.zig
@@ -12,163 +12,163 @@ const CameraData = extern struct {
     voxel: u64,
 };
 
-pub const App = struct {
-    window: glfw.Window,
-    allocator: std.heap.GeneralPurposeAllocator(.{}),
+pub const App = @This();
 
-    // gl stuff
-    framebuffer: gfx.Framebuffer,
-    pipeline: gfx.ComputePipeline,
-    buffer: gfx.PersistentMappedBuffer,
-    vox: gfx.Texture,
+window: glfw.Window,
+allocator: std.heap.GeneralPurposeAllocator(.{}),
 
-    /// camera
-    old_mouse_x: f64 = 0.0,
-    old_mouse_y: f64 = 0.0,
+// gl stuff
+framebuffer: gfx.Framebuffer,
+pipeline: gfx.ComputePipeline,
+buffer: gfx.PersistentMappedBuffer,
+vox: gfx.Texture,
 
-    position: zmath.F32x4 = zmath.f32x4(0.0, 0.0, -5.0, 0.0),
-    pitch: f32 = 0.0,
-    yaw: f32 = 0.0,
-    cam_mat: zmath.Mat = zmath.identity(),
+/// camera
+old_mouse_x: f64 = 0.0,
+old_mouse_y: f64 = 0.0,
 
-    pub fn init() !App {
-        const window = glfw.Window.create(1280, 720, "voxl", null, null, .{ .srgb_capable = true }) orelse @panic("Failed to open GLFW window.");
-        try gfx.init(window);
-        gfx.enableDebug();
+position: zmath.F32x4 = zmath.f32x4(0.0, 0.0, -5.0, 0.0),
+pitch: f32 = 0.0,
+yaw: f32 = 0.0,
+cam_mat: zmath.Mat = zmath.identity(),
 
-        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-        errdefer _ = gpa.deinit();
+pub fn init() !App {
+    const window = glfw.Window.create(1280, 720, "voxl", null, null, .{ .srgb_capable = true }) orelse @panic("Failed to open GLFW window.");
+    try gfx.init(window);
+    gfx.enableDebug();
 
-        var frame = gfx.Framebuffer.init(1280, 720, gfx.TextureFormat.RGBA8);
-        errdefer frame.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    errdefer _ = gpa.deinit();
 
-        const pipeline = try gfx.ComputePipeline.init(gpa.allocator(), "assets/shaders/draw.comp");
+    var frame = gfx.Framebuffer.init(1280, 720, gfx.TextureFormat.RGBA8);
+    errdefer frame.deinit();
 
-        var buff = gfx.PersistentMappedBuffer.init(gfx.BufferType.Uniform, @sizeOf(f32), gfx.BufferCreationFlags.MappableWrite | gfx.BufferCreationFlags.MappableRead);
-        var vx = gfx.Texture.init(gfx.TextureKind.Texture3D, gfx.TextureFormat.RGBA8, 32, 32, 32);
+    const pipeline = try gfx.ComputePipeline.init(gpa.allocator(), "assets/shaders/draw.comp");
 
-        buff.get(CameraData).*.voxel = vx.get_image_handle(gfx.TextureUsage.Read, null);
+    var buff = gfx.PersistentMappedBuffer.init(gfx.BufferType.Uniform, @sizeOf(f32), gfx.BufferCreationFlags.MappableWrite | gfx.BufferCreationFlags.MappableRead);
+    var vx = gfx.Texture.init(gfx.TextureKind.Texture3D, gfx.TextureFormat.RGBA8, 32, 32, 32);
 
-        return .{ .window = window, .allocator = gpa, .framebuffer = frame, .pipeline = pipeline, .buffer = buff, .vox = vx };
+    buff.get(CameraData).*.voxel = vx.get_image_handle(gfx.TextureUsage.Read, null);
+
+    return .{ .window = window, .allocator = gpa, .framebuffer = frame, .pipeline = pipeline, .buffer = buff, .vox = vx };
+}
+
+/// Called when the mouse is moved.
+pub fn on_mouse_moved(self: *@This(), xpos: f64, ypos: f64) void {
+    const delta_x = xpos - self.old_mouse_x;
+    const delta_y = ypos - self.old_mouse_y;
+
+    self.pitch = clamp(self.pitch + @as(f32, @floatCast(delta_y)) * 0.001, -std.math.pi / 2.0, std.math.pi / 2.0);
+    self.yaw = self.yaw + @as(f32, @floatCast(delta_x)) * 0.001;
+
+    self.cam_mat = zmath.matFromRollPitchYaw(self.pitch, self.yaw, 0.0);
+
+    self.old_mouse_x = xpos;
+    self.old_mouse_y = ypos;
+}
+
+/// Called upon window resize.
+pub fn on_resize(self: *@This(), width: u32, height: u32) void {
+    gfx.resize(width, height);
+    self.framebuffer.deinit();
+    self.framebuffer = gfx.Framebuffer.init(width, height, gfx.TextureFormat.RGBA8);
+}
+
+/// Called upon key down.
+pub fn on_key_down(self: *@This(), key: glfw.Key, scancode: i32, mods: glfw.Mods) void {
+    _ = mods;
+    _ = scancode;
+    switch (key) {
+        .r => self.reloadShaders(),
+        // camera controls
+        .w => {
+            self.position = self.position + zmath.mul(zmath.f32x4(0.0, 0.0, 1.0, 0.0), self.cam_mat);
+        },
+        .s => {
+            self.position = self.position - zmath.mul(zmath.f32x4(0.0, 0.0, 1.0, 0.0), self.cam_mat);
+        },
+        .a => {
+            self.position = self.position - zmath.mul(zmath.f32x4(1.0, 0.0, 0.0, 0.0), self.cam_mat);
+        },
+        .d => {
+            self.position = self.position + zmath.mul(zmath.f32x4(1.0, 0.0, 0.0, 0.0), self.cam_mat);
+        },
+        .space => {
+            self.position = self.position + zmath.f32x4(0.0, 1.0, 0.0, 0.0);
+        },
+        else => {},
     }
+}
 
-    /// Called when the mouse is moved.
-    pub fn on_mouse_moved(self: *@This(), xpos: f64, ypos: f64) void {
-        const delta_x = xpos - self.old_mouse_x;
-        const delta_y = ypos - self.old_mouse_y;
+/// Main app loop.
+pub fn run(self: *@This()) void {
+    self.window.setUserPointer(self);
 
-        self.pitch = clamp(self.pitch + @as(f32, @floatCast(delta_y)) * 0.001, -std.math.pi / 2.0, std.math.pi / 2.0);
-        self.yaw = self.yaw + @as(f32, @floatCast(delta_x)) * 0.001;
-
-        self.cam_mat = zmath.matFromRollPitchYaw(self.pitch, self.yaw, 0.0);
-
-        self.old_mouse_x = xpos;
-        self.old_mouse_y = ypos;
-    }
-
-    /// Called upon window resize.
-    pub fn on_resize(self: *@This(), width: u32, height: u32) void {
-        gfx.resize(width, height);
-        self.framebuffer.deinit();
-        self.framebuffer = gfx.Framebuffer.init(width, height, gfx.TextureFormat.RGBA8);
-    }
-
-    /// Called upon key down.
-    pub fn on_key_down(self: *@This(), key: glfw.Key, scancode: i32, mods: glfw.Mods) void {
-        _ = mods;
-        _ = scancode;
-        switch (key) {
-            .r => self.reloadShaders(),
-            // camera controls
-            .w => {
-                self.position = self.position + zmath.mul(zmath.f32x4(0.0, 0.0, 1.0, 0.0), self.cam_mat);
-            },
-            .s => {
-                self.position = self.position - zmath.mul(zmath.f32x4(0.0, 0.0, 1.0, 0.0), self.cam_mat);
-            },
-            .a => {
-                self.position = self.position - zmath.mul(zmath.f32x4(1.0, 0.0, 0.0, 0.0), self.cam_mat);
-            },
-            .d => {
-                self.position = self.position + zmath.mul(zmath.f32x4(1.0, 0.0, 0.0, 0.0), self.cam_mat);
-            },
-            .space => {
-                self.position = self.position + zmath.f32x4(0.0, 1.0, 0.0, 0.0);
-            },
-            else => {},
+    self.window.setFramebufferSizeCallback((struct {
+        pub fn handle_resize(window: glfw.Window, width: u32, height: u32) void {
+            const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
+            app.on_resize(width, height);
         }
-    }
+    }).handle_resize);
 
-    /// Main app loop.
-    pub fn run(self: *@This()) void {
-        self.window.setUserPointer(self);
-
-        self.window.setFramebufferSizeCallback((struct {
-            pub fn handle_resize(window: glfw.Window, width: u32, height: u32) void {
-                const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
-                app.on_resize(width, height);
+    self.window.setKeyCallback((struct {
+        pub fn handle_key(window: glfw.Window, key: glfw.Key, scancode: i32, action: glfw.Action, mods: glfw.Mods) void {
+            const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
+            if (action == .press or action == .repeat) {
+                app.on_key_down(key, scancode, mods);
             }
-        }).handle_resize);
-
-        self.window.setKeyCallback((struct {
-            pub fn handle_key(window: glfw.Window, key: glfw.Key, scancode: i32, action: glfw.Action, mods: glfw.Mods) void {
-                const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
-                if (action == .press or action == .repeat) {
-                    app.on_key_down(key, scancode, mods);
-                }
-            }
-        }).handle_key);
-
-        self.window.setCursorPosCallback((struct {
-            pub fn handle_mouse_move(window: glfw.Window, xpos: f64, ypos: f64) void {
-                const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
-                app.on_mouse_moved(xpos, ypos);
-            }
-        }).handle_mouse_move);
-
-        self.window.setInputModeCursor(glfw.Window.InputModeCursor.disabled);
-
-        while (!self.window.shouldClose()) {
-            glfw.pollEvents();
-            // app logic
-            self.update();
-            // render
-            self.draw();
-            // swap buffers and poll events
-            self.window.swapBuffers();
         }
-    }
+    }).handle_key);
 
-    pub fn update(self: *@This()) void {
-        self.buffer.get(CameraData).*.matrix = self.cam_mat;
-        self.buffer.get(CameraData).*.position = self.position;
-    }
+    self.window.setCursorPosCallback((struct {
+        pub fn handle_mouse_move(window: glfw.Window, xpos: f64, ypos: f64) void {
+            const app: *App = window.getUserPointer(App) orelse @panic("Failed to get user pointer.");
+            app.on_mouse_moved(xpos, ypos);
+        }
+    }).handle_mouse_move);
 
-    pub fn draw(self: *@This()) void {
-        const wsize = self.window.getFramebufferSize();
-        self.buffer.bind(1);
-        self.framebuffer.clear(0.0, 0.0, 0.0, 0.0);
-        self.framebuffer.color_attachment.bind_image(0, gfx.TextureUsage.Write, null);
-        self.pipeline.bind();
-        self.pipeline.dispatch(90, 80, 1);
-        gfx.clear(0.0, 0.0, 0.0);
-        self.framebuffer.blit_to_screen(0, 0, 0, 0, wsize.width, wsize.height);
-    }
+    self.window.setInputModeCursor(glfw.Window.InputModeCursor.disabled);
 
-    /// Reloads the shaders.
-    pub fn reloadShaders(self: *@This()) void {
-        const pipeline = gfx.ComputePipeline.init(self.allocator.allocator(), "assets/shaders/draw.comp") catch |err| {
-            std.log.warn("Failed to reload shaders: {}\n", .{err});
-            return;
-        };
-        self.pipeline.deinit();
-        self.pipeline = pipeline;
-        std.log.debug("Shaders reloaded", .{});
+    while (!self.window.shouldClose()) {
+        glfw.pollEvents();
+        // app logic
+        self.update();
+        // render
+        self.draw();
+        // swap buffers and poll events
+        self.window.swapBuffers();
     }
+}
 
-    pub fn deinit(self: *@This()) void {
-        self.framebuffer.deinit();
-        self.window.destroy();
-        _ = self.allocator.deinit();
-    }
-};
+pub fn update(self: *@This()) void {
+    self.buffer.get(CameraData).*.matrix = self.cam_mat;
+    self.buffer.get(CameraData).*.position = self.position;
+}
+
+pub fn draw(self: *@This()) void {
+    const wsize = self.window.getFramebufferSize();
+    self.buffer.bind(1);
+    self.framebuffer.clear(0.0, 0.0, 0.0, 0.0);
+    self.framebuffer.color_attachment.bind_image(0, gfx.TextureUsage.Write, null);
+    self.pipeline.bind();
+    self.pipeline.dispatch(90, 80, 1);
+    gfx.clear(0.0, 0.0, 0.0);
+    self.framebuffer.blit_to_screen(0, 0, 0, 0, wsize.width, wsize.height);
+}
+
+/// Reloads the shaders.
+pub fn reloadShaders(self: *@This()) void {
+    const pipeline = gfx.ComputePipeline.init(self.allocator.allocator(), "assets/shaders/draw.comp") catch |err| {
+        std.log.warn("Failed to reload shaders: {}\n", .{err});
+        return;
+    };
+    self.pipeline.deinit();
+    self.pipeline = pipeline;
+    std.log.debug("Shaders reloaded", .{});
+}
+
+pub fn deinit(self: *@This()) void {
+    self.framebuffer.deinit();
+    self.window.destroy();
+    _ = self.allocator.deinit();
+}
