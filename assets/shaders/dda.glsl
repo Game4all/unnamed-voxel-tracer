e3293d26e3b1c9097617e9aa6a56b465eebd0446
Lucas A <game4allyt@gmail.com>
2023-10-18 15:14:53 +0300

    Move stuff around

--- assets/shaders/dda.glsl
+++ assets/shaders/dda.glsl
@@ -18,3 +18,13 @@ void dda_step(inout ivec3 mapPos, inout vec3 deltaDist, in ivec3 rayStep, inout 
 float dda_distance(in vec3 rayDir, in vec3 deltaDist, in vec3 sideDist, in bvec3 mask) {
     return length(vec3(mask) * (sideDist - deltaDist)) / length(rayDir);
 }
+
+vec2 intersectAABB(vec3 rayOrigin, vec3 rayDir, vec3 boxMin, vec3 boxMax) {
+    vec3 tMin = (boxMin - rayOrigin) / rayDir;
+    vec3 tMax = (boxMax - rayOrigin) / rayDir;
+    vec3 t1 = min(tMin, tMax);
+    vec3 t2 = max(tMin, tMax);
+    float tNear = max(max(t1.x, t1.y), t1.z);
+    float tFar = min(min(t2.x, t2.y), t2.z);
+    return vec2(tNear, tFar);
+};
