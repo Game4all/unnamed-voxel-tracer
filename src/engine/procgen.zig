b55ea3b8800aaafc9661ece3e2bddaf2f2e2b6b7
Lucas A <game4allyt@gmail.com>
2023-10-21 20:26:22 +0300

    aa

--- src/engine/procgen.zig
+++ src/engine/procgen.zig
@@ -7,23 +7,7 @@ pub fn procgen(comptime dim: comptime_int, world: anytype) void {
             const val = gen.noise2(@as(f32, @floatFromInt(x)) / 10.0, @as(f32, @floatFromInt(z)) / 10.0);
             const vh: u32 = @intFromFloat(@max(val * @as(f32, @floatFromInt(dim)) * 0.1, 0.0));
             for (0..vh) |h| {
-                world.set(x, h, z, 0xFFFFFFFF);
-            }
-        }
-    }
-    spawn_trees(dim, world);
-}
-
-fn spawn_trees(comptime dim: comptime_int, world: anytype) void {
-    const gen = znoise.FnlGenerator{ .fractal_type = .ridged };
-    for (0..dim) |x| {
-        for (0..dim) |z| {
-            const val = gen.noise2(@as(f32, @floatFromInt(x)) / 2.0, @as(f32, @floatFromInt(z)) / 2.0);
-            const vh: u32 = @intFromFloat(@max(val * @as(f32, @floatFromInt(dim)) * 0.1, 0.0));
-            if (vh > 5) {
-                for (0..vh) |h| {
-                    world.set(x, h, z, 0xAAAFFFFF);
-                }
+                world.set(x, h, z, 0x0000ff00);
             }
         }
     }
