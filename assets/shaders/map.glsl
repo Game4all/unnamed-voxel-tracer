fa1e2ee32af2feb5c86ebf180a36f72dd282ce8f
Lucas A <game4allyt@gmail.com>
2023-10-21 20:42:57 +0300

    tweak

--- assets/shaders/map.glsl
+++ assets/shaders/map.glsl
@@ -12,6 +12,14 @@ layout(binding = 3) buffer mapData {
     uint chunks[];
 };
 
+
+uint map_getVoxelRaw(ivec3 pos) {
+    if (any(lessThan(pos, ivec3(0))) || any(greaterThanEqual(pos, ivec3(MAP_DIMENSION))))
+        return 0;
+
+    return data[pos.x + MAP_DIMENSION * (pos.y + pos.z * MAP_DIMENSION)];
+}
+
 vec4 map_getVoxel(ivec3 pos) {
     if (any(lessThan(pos, ivec3(0))) || any(greaterThanEqual(pos, ivec3(MAP_DIMENSION))))
         return vec4(0.);
@@ -29,7 +37,7 @@ uint map_getChunkFlags(ivec3 pos) {
 }
 
 
-bool traceMap(in vec3 rayOrigin, in vec3 rayDir, out vec3 color, out vec3 vmask, out ivec3 vmapPos, out float totalDistance, out ivec3 vrayStep) {
+bool traceMap(in vec3 rayOrigin, in vec3 rayDir, out vec3 vmask, out ivec3 vmapPos, out float totalDistance, out ivec3 vrayStep) {
     ivec3 chMapPos;
     vec3 chDeltaDist;
     ivec3 chRayStep;
@@ -38,7 +46,7 @@ bool traceMap(in vec3 rayOrigin, in vec3 rayDir, out vec3 color, out vec3 vmask,
 
     dda_init(rayOrigin / float(CHUNK_DIMENSION), rayDir, chMapPos, chDeltaDist, chRayStep, chSideDist, chMask);
 
-    for (int i = 0; i < 128; i++) {
+    for (int i = 0; i < 64; i++) {
 
         if (map_getChunkFlags(chMapPos) != 0) {
             vec3 updatedRayOrigin = rayOrigin + rayDir * dda_distance(rayDir, chDeltaDist, chSideDist, chMask) * float(CHUNK_DIMENSION) + 0.001;
@@ -51,10 +59,7 @@ bool traceMap(in vec3 rayOrigin, in vec3 rayDir, out vec3 color, out vec3 vmask,
             dda_init(updatedRayOrigin, rayDir, mapPos, deltaDist, rayStep, sideDist, mask);
 
             for (int i = 0; i < 24; i++) {
-                
-                vec4 voxel = map_getVoxel(mapPos);
-                if (length(voxel) > 0.) {
-                    color = voxel.rgb;
+                if (map_getVoxelRaw(mapPos) != 0) {
                     vmask = vec3(mask);
                     vmapPos = mapPos;
                     totalDistance = dda_distance(rayDir, chDeltaDist, chSideDist, chMask) * float(CHUNK_DIMENSION) + dda_distance(rayDir, deltaDist, sideDist, mask);
