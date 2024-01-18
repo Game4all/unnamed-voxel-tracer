6b6d18fde898cbc3ec190b715a3fc44c3d877d1c
Lucas A <game4allyt@gmail.com>
2024-01-18 14:25:31 +0100

    Add sound

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,15 @@
 const glfw = @import("mach_glfw");
 const app = @import("engine/app.zig");
+const std = @import("std");
 
 pub fn main() !void {
     _ = glfw.init(.{});
     defer glfw.terminate();
-    var application = try app.App.init();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var application = try app.App.init(gpa.allocator());
     defer application.deinit();
     application.run();
 }
