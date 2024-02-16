805359f721a08c9740ba01755d68a527603f99f7
Lucas A <game4allyt@gmail.com>
2024-02-16 22:01:49 +0100

    Fix shadows

--- assets/shaders/secondary.comp.glsl
+++ assets/shaders/secondary.comp.glsl
@@ -47,7 +47,7 @@ void main() {
 
     vec4 prev = imageLoad(frameIllumination, pixelCoords);
 
-    HitInfo inter = traceMap(rayOrigin, rayDir, 16);
+    HitInfo inter = traceMap(rayOrigin, rayDir, 32);
     vec4 illum = inter.is_hit ? vec4(rayDir.xyz, -0.3) : vec4(rayDir.xyz, 0.3);
 
     imageStore(frameIllumination, pixelCoords, mix(prev, illum, 0.5));
