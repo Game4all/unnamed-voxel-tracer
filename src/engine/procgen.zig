aad7be56f99fec732fc0b49c4cb8d71276948025
Lucas A <game4allyt@gmail.com>
2024-03-16 18:06:32 +0100

    Move LCG to its own file

--- src/engine/procgen.zig
+++ src/engine/procgen.zig
@@ -1,23 +1,10 @@
 const znoise = @import("znoise");
 const std = @import("std");
-
-pub const LCG = struct {
-    seed: u32,
-
-    pub inline fn rand(self: *@This()) u32 {
-        self.seed = @addWithOverflow(@mulWithOverflow(self.seed, 1103515245).@"0", 12345).@"0";
-
-        return self.seed;
-    }
-
-    pub inline fn rand_usize(self: *@This()) usize {
-        return @intCast(self.rand());
-    }
-};
+const rng = @import("rng.zig");
 
 pub fn procgen(comptime dim: comptime_int, world: anytype, offsetX: f32, offsetY: f32) void {
     const height_gen = znoise.FnlGenerator{ .fractal_type = .fbm };
-    var lcg = LCG{ .seed = 0x46AE4F };
+    var lcg = rng.LCG{ .seed = 0x46AE4F };
 
     for (0..dim) |x| {
         for (0..dim) |z| {
@@ -61,7 +48,7 @@ pub fn procgen(comptime dim: comptime_int, world: anytype, offsetX: f32, offsetY
     }
 }
 
-fn place_tree(prng: *LCG, world: anytype, x: usize, y: usize, z: usize) void {
+fn place_tree(prng: *rng.LCG, world: anytype, x: usize, y: usize, z: usize) void {
     const trunk_height = @mod(prng.rand_usize(), 4) + 4;
 
     world.set(x + 1, y, z + 1, 0x10000000 + 15);
